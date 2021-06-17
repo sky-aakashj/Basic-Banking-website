474ebcd189c2834f23f5282c48bdee4c4ae21b0c
Aakash Jaiswal <57659724+sky-aakashj@users.noreply.github.com>
2021-06-17 13:28:23 +0530

    Add files via upload
    final commit

---
+++ sideButton.php
@@ -0,0 +1,5 @@
+<form class="form-inline my-2 my-lg-0">
+        <a href="" class="btn btn-outline-success" data-toggle="tooltip" title="Your current Account Balance">Acount Balance : Rs.<?php echo $userData['balance']; ?></a>  
+       
+        <a href="logout.php" data-toggle="tooltip" title="Logout" class="btn btn-outline-danger mx-1" ><i class="fa fa-sign-out fa-fw"></i></a>    
+</form>
